0c35ce883b1aa19c5088111ce29c3a7e8354808e
Nox <103855043+Nyxious@users.noreply.github.com>
2025-12-01 13:04:41 -0500

    Update eye-tracker.py

--- V1/eye-tracker.py
+++ V1/eye-tracker.py
@@ -6,7 +6,7 @@ eye_cascade = cv2.CascadeClassifier("eye_data.xml")
 while True:
     ret, frame = video.read()
     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-    detection = eye_cascade.detectMultiScale(frame, minSize=(100, 100))
+    detection = eye_cascade.detectMultiScale(frame, minSize=(100, 100)) # Lower min size for detection on further back objects [Issue: lousier detection]
     for (x, y, w, h) in detection:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 0), 2)
     if (len(detection) > 0):
