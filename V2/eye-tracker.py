176018a434bcec962b6b13c87bb7d59b166f08ad
Nox <103855043+Nyxious@users.noreply.github.com>
2025-12-10 15:51:42 -0500

    Update eye-tracker.py

--- V2/eye-tracker.py
+++ V2/eye-tracker.py
@@ -1,24 +1,40 @@
 import cv2
-
+import pigpio
+import math
 modelWeights = "/home/pi/Downloads/modelFiles/frozen_inference_graph.pb"
 modelConfig = "/home/pi/Downloads/modelFiles/mobileNetV3.pbtxt"
 modelNames = "/home/pi/Downloads/modelFiles/coco.names"
-video = cv2.VideoCapture("/dev/video0", cv2.CAP_V4L2)
 nameArray = []
+focalLen = 32
+video = cv2.VideoCapture("/dev/video0", cv2.CAP_V4L2)
+pi = pigpio.pi()
 with open(modelNames, "rt") as f:
     nameArray = f.read().rstrip("\n").split("\n")
 model = cv2.dnn_DetectionModel(modelWeights, modelConfig)
 model.setInputParams(1.0 / 127.5, (320, 320), (127.5, 127.5, 127.5), True)
+def getAngleConversion(rads):
+    rads = rads + (math.pi / 2)
+    conversion = (2000 / math.pi) * rads + 500
+    return conversion
 while True:
     success, frame = video.read()
     classIds, confidence, box = model.detect(frame, confThreshold=0.6, nmsThreshold=0.7)
+    print(len(classIds)) # Testing to check if theres anything inside of the classIds array
     if (len(classIds) > 0):
+    
         for (objectId, c) in zip(classIds.flatten(), box):
-            cv2.putText(frame, nameArray[objectId - 1],((c[0] + c[2])//2, (c[1] + c[3])//2), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 0 ,0), 5, cv2.LINE_AA, False)
-            cv2.rectangle(frame, (c[0], c[1]), (c[0] + c[2], c[1] + c[3]), (255, 0 ,0), 5)
-            cv2.imwrite("feed.jpg", frame)
-            break
+            
+            print(nameArray[objectId - 1]) # Prints the object detected using the coco list
+            if (nameArray[objectId - 1] == "person"):
+                #new2
+                coordinateX, widthCenter = (c[0] + (c[2]/2)), frame.shape[1] // 2
+                print(coordinateX, widthCenter)
+                angle = getAngleConversion(math.atan((coordinateX - widthCenter) / focalLen))
+                print(angle)
+                pi.set_servo_pulsewidth(22, angle)
+                cv2.imwrite(nameArray[objectId - 1]+".jpg", frame)
+                
+                break
+            
     if not success:
         break
-    
-
